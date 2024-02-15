d3755237c50a664964a32e771bd9350bef9ef881
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2024-02-15 19:40:42 +0300

    feat: data-statistics module initialized

---
+++ data-statistics/src/main/java/isthatkirill/datastatistics/DataStatisticsApplication.java
@@ -0,0 +1,18 @@
+package isthatkirill.datastatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kirill Emelyanov
+ */
+
+@SpringBootApplication
+public class DataStatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStatisticsApplication.class, args);
+    }
+
+
+}
