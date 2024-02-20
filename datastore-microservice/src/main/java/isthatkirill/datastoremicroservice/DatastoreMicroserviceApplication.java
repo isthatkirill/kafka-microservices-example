5df21c8d6ea39585a66b636f31b97a07979c3082
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2024-02-20 22:02:26 +0300

    feat: new module added

---
+++ datastore-microservice/src/main/java/isthatkirill/datastoremicroservice/DatastoreMicroserviceApplication.java
@@ -0,0 +1,18 @@
+package isthatkirill.datastoremicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kirill Emelyanov
+ */
+
+@SpringBootApplication
+public class DatastoreMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatastoreMicroserviceApplication.class, args);
+    }
+
+
+}
